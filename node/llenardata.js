ea619e58c454aa457b7f8f602d4f1b22a5ec97fc
jorge <jumanor@gmail.com>
2015-07-12 12:56:27 -0500

    Usamos async.js para verificar la ejecucion de scripts

--- node/llenardata.js
+++ node/llenardata.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var async=require("async");
 
 mongoose.connect('mongodb://localhost/sisacademico',function(){
     
@@ -15,36 +16,58 @@ var UsuarioDB=require('./modelosDB/usuario.js');
 
 function cargarData(){
 	console.log("Iniciando Carga");
+    async.parallel([
+        function(callback){
+           
+            var tmp=new UsuarioDB();
+            tmp.usuario="jumanor";
+            tmp.contrasenia="jumanor";
+            tmp.perfil="Alumno";
 
-	var tmp=new UsuarioDB();
-	tmp.usuario="jumanor";
-	tmp.contrasenia="jumanor";
-	tmp.perfil="Alumno";
+            tmp.save(function(err){
+                if(err)console.log(err);
+                console.log("OK "+tmp.id);
+                callback(null,null);
+            });
+            
+        },
+        function(callback){
+           
+            var tmp=new UsuarioDB();
+            tmp.usuario="jumanor";
+            tmp.contrasenia="jumanor";
+            tmp.perfil="Profesor";
 
-	tmp.save(function(err){
-		if(err)console.log(err);
-		console.log("OK");
-	});
+            tmp.save(function(err){
+                if(err)console.log(err);
+                console.log("OK "+tmp.id);
+                callback(null,null);
+            });
+            
+        },
+        function(callback){
+            
+            var tmp=new UsuarioDB();
+            tmp.usuario="jumanor";
+            tmp.contrasenia="jumanor";
+            tmp.perfil="Administrador";
 
-	var tmp=new UsuarioDB();
-	tmp.usuario="jumanor";
-	tmp.contrasenia="jumanor";
-	tmp.perfil="Profesor";
-
-	tmp.save(function(err){
-		if(err)console.log(err);
-		console.log("OK");
-	});
-
-	var tmp=new UsuarioDB();
-	tmp.usuario="jumanor";
-	tmp.contrasenia="jumanor";
-	tmp.perfil="Administrador";
-
-	tmp.save(function(err){
-		if(err)console.log(err);
-		console.log("OK");
-	});
-
+            tmp.save(function(err){
+                if(err)console.log(err);
+                console.log("OK "+tmp.id);
+                callback(null,null);
+            });
+            
+        }
+    ],
+    // optional callback
+    function(err, results){
+       if(err){
+            console.log("Hubo un error "+err);
+        }
+        else{
+             console.log("Carga Finalizada");
+        }
+    });
 	
 }
